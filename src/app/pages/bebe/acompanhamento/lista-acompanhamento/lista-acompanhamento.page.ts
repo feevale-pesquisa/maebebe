98be00d521aad94364583be5a2a279824ee001c1
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2020-07-04 23:57:00 -0300

    Criando listagem do acompanhamento do bebe

--- src/app/pages/bebe/acompanhamento/lista-acompanhamento/lista-acompanhamento.page.ts
+++ src/app/pages/bebe/acompanhamento/lista-acompanhamento/lista-acompanhamento.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BuscaMaeService } from 'src/app/services/busca/busca-mae.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-lista-acompanhamento',
@@ -11,19 +13,61 @@ export class ListaAcompanhamentoPage implements OnInit {
   public idMae: string
   public idGestacao: string
   public idBebe: string
+  public bebe: Object = null
+  public acompanhamentos: any = []
+  public carregando: boolean = false
+
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private maeServico: BuscaMaeService
   ) { }
 
   ngOnInit() {
+    
   }
 
   ionViewWillEnter() {
     this.idMae = this.route.snapshot.paramMap.get('id_mae')
     this.idGestacao = this.route.snapshot.paramMap.get('id_gestacao')
     this.idBebe = this.route.snapshot.paramMap.get('id_bebe')
+
+    this.carregarDados()
+  }
+
+  atualizar(event) {
+    this.carregarAcompanhamentos().then(() => {
+      event.target.complete()
+    })
+  }
+
+  async carregarDados() {
+    await this.carregarBebe()
+    if(this.bebe)
+      await this.carregarAcompanhamentos()
+  }
+
+  async carregarBebe() {
+    this.carregando = true
+    try {
+      this.bebe = await this.maeServico.buscarBebePorId(this.idBebe)
+      this.carregando = false
+    } catch(error) {
+      this.carregando = false
+      this.voltar()
+    }
+  }
+
+  async carregarAcompanhamentos() {
+    this.carregando = true
+    try {
+      this.acompanhamentos = await this.maeServico.buscarAcompanhamentoPorBebe(this.idBebe)
+      this.carregando = false
+    } catch(error) {
+      this.carregando = false
+      this.voltar()
+    }
   }
 
   abrirCadastroAcompanhamento() {
@@ -34,8 +78,19 @@ export class ListaAcompanhamentoPage implements OnInit {
     ])
   }
 
+  abrirEdicaoAcompanhamento() {
+
+  }
+
   voltar() {
     this.router.navigate(["mae", this.idMae, "gestacao", this.idGestacao, "bebe"])
+  }
+
+  formatarData(data) {
+    if(data.length == 10)
+      return data
+      
+    return moment(data).isValid() ? moment(data).format('DD/MM/YYYY') : ''
   }
 
 }
