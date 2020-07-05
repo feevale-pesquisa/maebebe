0dcb869bc563da6d827a03362f0c64c18f68f0ea
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2020-07-04 23:56:09 -0300

    Estrutura pra salvar e buscar sem internet parte 1 - Criando metodos na cadastro-mae.service.ts especificos para cadastrar o acompanhamento. Adicionando no agendamento para salvar quando não tiver internet

--- src/app/services/formulario/cadastro-mae.service.ts
+++ src/app/services/formulario/cadastro-mae.service.ts
@@ -138,11 +138,37 @@ export class CadastroMaeService {
         return id
     }
 
+    public async cadastrarAcompanhamentoBebe(idMae, idGestacao, idBebe, dados:any)
+    {
+        idMae = this.verificarId(idMae)
+        idGestacao = this.verificarId(idGestacao)
+        idBebe = this.verificarId(idBebe)
+
+        if(this.possuiInternet && !this.ehIdTemporario(idMae) && !this.ehIdTemporario(idGestacao) && !this.ehIdTemporario(idBebe)) {
+            try {
+                let resposta: {id: any} = await this.api.salvarFormularioAcompanhamentoBebe(idMae, idGestacao, idBebe, dados)
+                return resposta.id
+            } catch (error) {
+                if(error instanceof HttpErrorResponse)
+                    console.debug('[cadastro-mae.service.ts] - Erro ao salvar mãe ' + dados.nome + ', salvando em cache')
+                else
+                    throw error
+            }
+        }
+
+        let id = moment().format('HH:mm:ss.SSS')
+
+        dados.id_bebe_acompanhamento = id
+
+        await this.cache.add(id, dados, CacheType.CADASTRO_ACOMPANHAMENTO_BEBE, 100000)
+
+        return id
+    }
+
     private async salvarMaes()
     {
+        if(this.bloquearAgendamento) return
         let maes:Array<any> = await this.cache.getByType(CacheType.CADASTRO_MAE)
-        let gestacoes:Array<any> = await this.cache.getByType(CacheType.CADASTRO_GESTACAO)
-        let bebes:Array<any> = await this.cache.getByType(CacheType.CADASTRO_BEBE)
 
         for (let mae of maes) {
             try {
@@ -157,20 +183,10 @@ export class CadastroMaeService {
 
                 this.idsSalvos[mae.id] = id
 
-                let gestacoesPorMae = gestacoes.filter(gestacao => { return gestacao.id_mae = mae.id })
-                let bebesPorMae = bebes.filter(bebe => { return bebe.id_mae = mae.id })
-
-                for (let gestacao of gestacoesPorMae) {
-                    gestacao.id_mae = id
-
-                    await this.cache.add(gestacao.id_gestacao, gestacao, CacheType.CADASTRO_GESTACAO, 100000)
-                }
-
-                for (let bebe of bebesPorMae) {
-                    bebe.id_mae = id
-
-                    await this.cache.add(bebe.id_bebe, bebe, CacheType.CADASTRO_BEBE, 100000)
-                }
+                this.atualizarIdNoCache(mae.id, id, CacheType.CADASTRO_GESTACAO, 'id_mae', 'id_gestacao')
+                this.atualizarIdNoCache(mae.id, id, CacheType.CADASTRO_BEBE, 'id_mae', 'id_bebe')
+                this.atualizarIdNoCache(mae.id, id, CacheType.CADASTRO_ACOMPANHAMENTO_BEBE, 'id_mae', 'id_bebe_acompanhamento')
+                
             } catch (error) {
                 if(error instanceof FormException) {
                     
@@ -187,8 +203,8 @@ export class CadastroMaeService {
 
     private async salvarGestacoes()
     {
+        if(this.bloquearAgendamento) return
         let gestacoes:Array<any> = await this.cache.getByType(CacheType.CADASTRO_GESTACAO)
-        let bebes:Array<any> = await this.cache.getByType(CacheType.CADASTRO_BEBE)
 
         for (let gestacao of gestacoes) {
             try {
@@ -205,13 +221,8 @@ export class CadastroMaeService {
     
                 this.idsSalvos[gestacao.id_gestacao] = id
     
-                let bebesPorMae = bebes.filter(bebe => { return bebe.id_gestacao = gestacao.id_gestacao })
-    
-                for (let bebe of bebesPorMae) {
-                    bebe.id_gestacao = id
-    
-                    await this.cache.add(bebe.id_bebe, bebe, CacheType.CADASTRO_BEBE, 100000)
-                }
+                this.atualizarIdNoCache(gestacao.id_gestacao, id, CacheType.CADASTRO_BEBE, 'id_gestacao', 'id_bebe')
+                this.atualizarIdNoCache(gestacao.id_gestacao, id, CacheType.CADASTRO_ACOMPANHAMENTO_BEBE, 'id_gestacao', 'id_bebe_acompanhamento')
 
                 await this.cache.removeById(gestacao.id_mae, CacheType.LISTA_GESTACAO)
                 
@@ -233,6 +244,7 @@ export class CadastroMaeService {
 
     private async salvarBebes()
     {
+        if(this.bloquearAgendamento) return
         let bebes:Array<any> = await this.cache.getByType(CacheType.CADASTRO_BEBE)
 
         for (let bebe of bebes) {
@@ -245,10 +257,12 @@ export class CadastroMaeService {
                 if(this.ehIdTemporario(bebe.id_mae) || this.ehIdTemporario(bebe.id_gestacao))
                     continue
 
-                let resposta: {id: any} = await this.api.salvarFormularioBebe(bebe.id_mae, bebe.id_gestacao, bebe)
-                let id = resposta.id
+                let resposta: {id_bebe: any} = await this.api.salvarFormularioBebe(bebe.id_mae, bebe.id_gestacao, bebe)
+                let id = resposta.id_bebe
                 
                 this.idsSalvos[bebe.id_bebe] = id
+
+                this.atualizarIdNoCache(bebe.id_bebe, id, CacheType.CADASTRO_ACOMPANHAMENTO_BEBE, 'id_bebe', 'id_bebe_acompanhamento')
 
                 await this.cache.removeById(bebe.id_bebe, CacheType.CADASTRO_BEBE)
                 await this.cache.removeById(bebe.id_gestacao, CacheType.LISTA_BEBE)
@@ -269,11 +283,64 @@ export class CadastroMaeService {
         }
     }
 
+    private async salvarAcompanhamentosBebes()
+    {
+        if(this.bloquearAgendamento) return
+        let acompanhamentos:Array<any> = await this.cache.getByType(CacheType.CADASTRO_ACOMPANHAMENTO_BEBE)
+
+        for (let acompanhamento of acompanhamentos) {
+            try {
+                
+                if(acompanhamento.possuiErro || acompanhamento.id_bebe_acompanhamento == undefined || this.bloquearAgendamento) {
+                    continue
+                }
+
+                if(this.ehIdTemporario(acompanhamento.id_bebe))
+                    continue
+
+                let resposta: {id: any} = await this.api.salvarFormularioAcompanhamentoBebe(
+                    acompanhamento.id_mae, acompanhamento.id_gestacao, acompanhamento.id_bebe, acompanhamento
+                )
+                let id = resposta.id
+                
+                this.idsSalvos[acompanhamento.id_bebe_acompanhamento] = id
+
+                await this.cache.removeById(acompanhamento.id_bebe_acompanhamento, CacheType.CADASTRO_ACOMPANHAMENTO_BEBE)
+                await this.cache.removeById(acompanhamento.id_bebe, CacheType.LISTA_ACOMPANHAMENTO_BEBE)
+
+            } catch (error) {
+                
+                if(error instanceof FormException) {
+                    
+                    acompanhamento.possuiErro = true
+                    acompanhamento.erros = error.getErrors()
+                    await this.cache.add(acompanhamento.id_bebe_acompanhamento, acompanhamento, CacheType.CADASTRO_ACOMPANHAMENTO_BEBE, 100000)
+
+                } else {
+                    throw error
+                }
+
+            }
+        }
+    }
+
+    private async atualizarIdNoCache(idAntigo, idNovo, tipo, campo, campoKey) {
+        let listaCache:Array<any> = await this.cache.getByType(tipo)
+        listaCache = listaCache.filter(itemCache => { return itemCache[campo] == idAntigo })
+
+        for (let item of listaCache) {
+            console.log(item[campo], idNovo, item[campoKey])
+            item[campo] = idNovo
+            await this.cache.add(item[campoKey], item, tipo, 100000)
+        }
+    }
+
     public async sincronizar()
     {
         await this.salvarMaes()
         await this.salvarGestacoes()
         await this.salvarBebes()
+        await this.salvarAcompanhamentosBebes()
     }
 
     public async agendar(numero = 1, tempo = 10000)
